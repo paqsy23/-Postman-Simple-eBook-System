82e1cf825739d1c4184f343a73ec71a730b7f7f2
Paqsy Jalasukma <aci.jalasukma@gmail.com>
2020-05-24 19:20:14 +0700

    mau pull

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const express = require('express');
+const express = require("express"), cors = require("cors");
+const bookshelf = require("./routes/bookshelf");
 const app = express();
 
+app.use(cors());
 app.use(express.urlencoded({extended:true}));
-app.use("/bookshelf", require("./routes/bookshelf"));
+app.use("/bookshelf", bookshelf);
 
 app.listen(3000, () => {
     console.log("Listening on port 3000...");
--- routes/bookshelf.js
+++ routes/bookshelf.js
@@ -1,11 +1,29 @@
-const express = require("express"), mysql = require("mysql");
+const express = require("express"), mysql = require("mysql"), request = require("request");
+const parseString = require("xml2js").parseString;
 const config = require("../config");
 const router = express.Router();
 const conn = mysql.createConnection(config.database);
 
-//Insert book to bookshelf
-router.post("/insert", (req, res) => {
+function getBook(id) {
+    return new Promise((resolve, reject) => {
+        const options = {
+            method: "GET",
+            url: `https://www.goodreads.com/book/show/${id}.xml?key=P1vzTR40liY9SzWSOOlSA`,
+            headers: { 'Content-Type': "application/x-www-form-urlencoded" }
+        }
+        request(options, (error, response) => {
+            if (error) reject(new Error(error));
+            else {
+                resolve(response.body);
+            }
+        })
+    });
+}
 
+//Insert book to bookshelf
+router.post("/insert", async (req, res) => {
+    const book = await getBook(50);
+    res.send(book);
 });
 
 //Show user's bookshelf
@@ -27,3 +45,5 @@ router.put("/changePrivacy", (req, res) => {
 router.delete("/delete", (req, res) => {
 
 });
+
+module.exports = router;
