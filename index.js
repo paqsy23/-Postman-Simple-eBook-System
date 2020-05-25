b2aab721f63512f1358c8319d26f49751d8aa217
Vincent <vincentchung_191999@yahoo.com>
2020-05-25 17:01:11 +0700

    user

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express"), cors = require("cors");
 const bookshelf = require("./routes/bookshelf");
 const review = require("./routes/review");
 const bookmark = require("./routes/bookmark");
+const user = require("./routes/user.js")
 const app = express();
 
 app.use(cors());
@@ -9,6 +10,7 @@ app.use(express.urlencoded({extended:true}));
 app.use("/bookshelf", bookshelf);
 app.use("/review", review);
 app.use("/bookmark", bookmark);
+app.use("/user",user);
 
 app.listen(3000, () => {
     console.log("Listening on port 3000...");
