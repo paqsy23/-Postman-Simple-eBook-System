9b5290d242382e6f205bcd9e5a48454d4d3f511b
stefanruben23 <stefanruben23@gmail.com>
2020-05-25 11:48:19 +0700

    bookmark

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const app = express();
 
 const bookshelf = require("./routes/bookshelf");
 const review = require("./routes/review");
+const bookmark = require("./routes/bookmark");
 
 app.use(express.urlencoded({extended:true}));
 app.use("/bookshelf", bookshelf);
 app.use("/review", review);
+app.use("/bookmark", bookmark);
 
 app.listen(3000, () => {
     console.log("Listening on port 3000...");
