3e802b01a98e9a7473515a309d88888933054255
patricktjendika <patricktjendika2521999@gmail.com>
2020-06-17 00:16:45 +0700

    Done

--- db_proyek_soa.sql
+++ db_proyek_soa.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 4.9.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 30, 2020 at 08:16 AM
--- Server version: 10.4.6-MariaDB
--- PHP Version: 7.3.9
+-- Waktu pembuatan: 16 Jun 2020 pada 18.41
+-- Versi server: 10.4.8-MariaDB
+-- Versi PHP: 7.3.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -27,9 +27,10 @@ USE `db_proyek_soa`;
 -- --------------------------------------------------------
 
 --
--- Table structure for table `bookmark`
+-- Struktur dari tabel `bookmark`
 --
 
+DROP TABLE IF EXISTS `bookmark`;
 CREATE TABLE `bookmark` (
   `username` varchar(15) NOT NULL,
   `id_book` int(11) NOT NULL,
@@ -40,28 +41,22 @@ CREATE TABLE `bookmark` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `d_bookshelf`
+-- Struktur dari tabel `d_bookshelf`
 --
 
+DROP TABLE IF EXISTS `d_bookshelf`;
 CREATE TABLE `d_bookshelf` (
   `username` varchar(15) NOT NULL,
   `id_book` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `d_bookshelf`
---
-
-INSERT INTO `d_bookshelf` (`username`, `id_book`) VALUES
-('tester', 40),
-('vincent', 40);
-
 -- --------------------------------------------------------
 
 --
--- Table structure for table `d_playlist`
+-- Struktur dari tabel `d_playlist`
 --
 
+DROP TABLE IF EXISTS `d_playlist`;
 CREATE TABLE `d_playlist` (
   `id_playlist` varchar(10) NOT NULL,
   `id_book` int(11) NOT NULL
@@ -70,42 +65,38 @@ CREATE TABLE `d_playlist` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `h_bookshelf`
+-- Struktur dari tabel `h_bookshelf`
 --
 
+DROP TABLE IF EXISTS `h_bookshelf`;
 CREATE TABLE `h_bookshelf` (
   `username` varchar(15) NOT NULL,
   `type` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `h_bookshelf`
---
-
-INSERT INTO `h_bookshelf` (`username`, `type`) VALUES
-('tester', 0),
-('u1', 1),
-('vincent', 0);
-
 -- --------------------------------------------------------
 
 --
--- Table structure for table `h_playlist`
+-- Struktur dari tabel `h_playlist`
 --
 
+DROP TABLE IF EXISTS `h_playlist`;
 CREATE TABLE `h_playlist` (
   `id_playlist` varchar(10) NOT NULL,
   `username` varchar(15) NOT NULL,
   `name` varchar(100) NOT NULL,
-  `type` int(1) NOT NULL
+  `type` int(1) NOT NULL,
+  `created_date` datetime NOT NULL,
+  `modified_date` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `review`
+-- Struktur dari tabel `review`
 --
 
+DROP TABLE IF EXISTS `review`;
 CREATE TABLE `review` (
   `id_book` int(11) NOT NULL,
   `username` varchar(15) NOT NULL,
@@ -116,9 +107,10 @@ CREATE TABLE `review` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Struktur dari tabel `user`
 --
 
+DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `username` varchar(15) NOT NULL,
   `password` varchar(50) NOT NULL,
@@ -126,102 +118,94 @@ CREATE TABLE `user` (
   `phone_number` varchar(20) NOT NULL,
   `type` int(1) NOT NULL,
   `profile_picture` varchar(100) NOT NULL,
-  `transaction_id` varchar(255) NOT NULL
+  `transaction_id` varchar(255) NOT NULL,
+  `status` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `user`
---
-
-INSERT INTO `user` (`username`, `password`, `name`, `phone_number`, `type`, `profile_picture`, `transaction_id`) VALUES
-('tester', '1', 'Vincent', '087819111575', 0, '1590487381223.png', ''),
-('u1', 'vc', 'Vincent', '087819111575', 1, '1590486501610.png', ''),
-('vincent', 'vc', 'Vincent', '087819111575', 1, '1590486501610.png', '');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `bookmark`
+-- Indeks untuk tabel `bookmark`
 --
 ALTER TABLE `bookmark`
   ADD PRIMARY KEY (`username`,`id_book`);
 
 --
--- Indexes for table `d_bookshelf`
+-- Indeks untuk tabel `d_bookshelf`
 --
 ALTER TABLE `d_bookshelf`
   ADD PRIMARY KEY (`username`,`id_book`);
 
 --
--- Indexes for table `d_playlist`
+-- Indeks untuk tabel `d_playlist`
 --
 ALTER TABLE `d_playlist`
   ADD PRIMARY KEY (`id_playlist`,`id_book`);
 
 --
--- Indexes for table `h_bookshelf`
+-- Indeks untuk tabel `h_bookshelf`
 --
 ALTER TABLE `h_bookshelf`
   ADD PRIMARY KEY (`username`);
 
 --
--- Indexes for table `h_playlist`
+-- Indeks untuk tabel `h_playlist`
 --
 ALTER TABLE `h_playlist`
   ADD PRIMARY KEY (`id_playlist`),
   ADD KEY `fk_h_playlist_username` (`username`);
 
 --
--- Indexes for table `review`
+-- Indeks untuk tabel `review`
 --
 ALTER TABLE `review`
   ADD PRIMARY KEY (`id_book`,`username`),
   ADD KEY `fk_review_username` (`username`);
 
 --
--- Indexes for table `user`
+-- Indeks untuk tabel `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`username`);
 
 --
--- Constraints for dumped tables
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
 --
 
 --
--- Constraints for table `bookmark`
+-- Ketidakleluasaan untuk tabel `bookmark`
 --
 ALTER TABLE `bookmark`
   ADD CONSTRAINT `fk_bookmark_username` FOREIGN KEY (`username`) REFERENCES `user` (`username`);
 
 --
--- Constraints for table `d_bookshelf`
+-- Ketidakleluasaan untuk tabel `d_bookshelf`
 --
 ALTER TABLE `d_bookshelf`
   ADD CONSTRAINT `fk_d_bookshelf_username` FOREIGN KEY (`username`) REFERENCES `user` (`username`);
 
 --
--- Constraints for table `d_playlist`
+-- Ketidakleluasaan untuk tabel `d_playlist`
 --
 ALTER TABLE `d_playlist`
   ADD CONSTRAINT `fk_d_playlist_id_playlist` FOREIGN KEY (`id_playlist`) REFERENCES `h_playlist` (`id_playlist`);
 
 --
--- Constraints for table `h_bookshelf`
+-- Ketidakleluasaan untuk tabel `h_bookshelf`
 --
 ALTER TABLE `h_bookshelf`
   ADD CONSTRAINT `fk_h_bookshelf_username` FOREIGN KEY (`username`) REFERENCES `user` (`username`);
 
 --
--- Constraints for table `h_playlist`
+-- Ketidakleluasaan untuk tabel `h_playlist`
 --
 ALTER TABLE `h_playlist`
   ADD CONSTRAINT `fk_h_playlist_username` FOREIGN KEY (`username`) REFERENCES `user` (`username`);
 
 --
--- Constraints for table `review`
+-- Ketidakleluasaan untuk tabel `review`
 --
 ALTER TABLE `review`
   ADD CONSTRAINT `fk_review_username` FOREIGN KEY (`username`) REFERENCES `user` (`username`);
